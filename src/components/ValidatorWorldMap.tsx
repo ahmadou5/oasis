ca7f96d2aea7e532384b63e6ab10d272ed38ad1f
ahmadou5 <ahmadlasauwal@gmail.com>
2025-12-10 06:15:24 +0100

    touch validatormap

--- src/components/ValidatorWorldMap.tsx
+++ src/components/ValidatorWorldMap.tsx
@@ -4,7 +4,7 @@ import React, { useState, useEffect } from "react";
 import { MapPin, Crown, Clock, Users, Globe } from "lucide-react";
 
 import { useValidators } from "../hooks/useValidators";
-import { ValidatorInfo } from "../types";
+import { ValidatorInfo } from "../store/slices/validatorSlice";
 
 interface ValidatorLocation extends ValidatorInfo {
   latitude?: number;
